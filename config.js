a567a0c29d7979f94a89f24f0cca5ed633765d57
mavedomedia-droid <mavedomedia@gmail.com>
2026-08-15 14:17:40 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+// Central configuration for WeTalkSound pages
+var CONFIG = {
+    // Google Apps Script Web App URL
+    // Replace with your actual deployment URL
+    GAS_URL: ''
+};
