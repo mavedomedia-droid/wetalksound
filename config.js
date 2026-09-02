4f9d3a94a8b62674693658ba3b9bdc351bf08710
mavedomedia-droid <mavedomedia@gmail.com>
2026-09-02 19:56:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 var CONFIG = {
     // Google Apps Script Web App URL
     // Replace with your actual deployment URL
-    GAS_URL: 'https://script.google.com/macros/s/AKfycby-4FmhoSS_GgU3yZHfbypSB2wM1ttLIgxneR3CPCD7kGpc1PF1_QV61XMh-J911FZx/exec'
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbzTo3RezWTf0IAYYTELozK2wYPqju4weRzr9g4K6LTFjWsLRE0fDaa4Uq0d_ljCrCy_/exec'
 };
